de778e6312895beba3ce75e8dd32394c2f2e41fc
GUILHERME SANTOS NASCIMENTO <guilherme.snascime10@senacsp.edu.br>
2026-07-02 15:14:23 -0300

    array-mercado-

--- atv/ArrayMercado.cs
+++ atv/ArrayMercado.cs
@@ -23,13 +23,40 @@ namespace CursoCHsharp.atv {
             for (int i = 0; i < produtos.Length; i++) {
                 Console.WriteLine($"{produtos[i]} - R$ {precos[i]}");
             }
+            double maiorPreco = precos[0];
+            string produtoMaisCaro = produtos[0];
+
+            for (int i = 1; i < precos.Length; i++) {
+                if (precos[i] > maiorPreco) {
+                    maiorPreco = precos[i];
+                    produtoMaisCaro = produtos[i];
+                }
+            }
+            
+            double menorPreco = precos[0];
+            string produtoMaisBarato = produtos[0];
+            
+            for(int i = 1; i < precos.Length; i++) {
+                if (precos[i] < menorPreco) {
+                    menorPreco = precos[i];
+                    produtoMaisBarato = produtos[i];
+                }
+            }
 
             double somaPreco = 0;
 
             foreach (double preco in precos) {
                 somaPreco += preco;
             }
+            double media = somaPreco / precos.Length;
+
+            Console.WriteLine($"Produtos cadastrado foi {produtos.Length} ");
             Console.WriteLine($"Total dos gastos no Mercado : {somaPreco:F2}");
+            
+           
+            Console.WriteLine($"Produto com maior valor {produtoMaisCaro} e o valor é {maiorPreco}");
+            Console.WriteLine($"Produto com menor valor {produtoMaisBarato} e o valor é {menorPreco}");
+            Console.WriteLine($"Media foi dos produtos gastados foi de {media}");
 
         }
     }
